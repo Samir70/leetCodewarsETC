1e5256a70645e42d85f507a45c854dae1599b40a
Samir70 <sbetmouni@talktalk.net>
2020-07-23 21:54:14 +0100

    corrected array format in openSenior

--- 1Python/openSenior.py
+++ 1Python/openSenior.py
@@ -3,9 +3,7 @@ def isSenior(arr):
         return "Senior"
     return "Open"
 
-members = [
-    [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9], [56, 4]]
-]
+members = [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9], [56, 4]]
 
 status = ['Senior' if age>=55 and handicap>7 else 'Open' for age, handicap in members]
 print(members)
