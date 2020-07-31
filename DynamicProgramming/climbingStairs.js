8b5c0552636363b54cefd2c0bd478c186f0418f7
Samir70 <sbetmouni@talktalk.net>
2020-07-31 10:17:34 +0100

    count ways to climb stairs

---
+++ DynamicProgramming/climbingStairs.js
@@ -0,0 +1,28 @@
+// How many ways to climb n steps 
+// if you can take either one or two steps each turn
+var climbStairs = function (n) {
+    var waysForIdx = [0, 1, 2];
+    var i = 3;
+    while (i <= n) {
+        var afterOneStep = waysForIdx[i - 1];
+        var afterTwoSteps = waysForIdx[i - 2];
+        waysForIdx.push(afterOneStep + afterTwoSteps);
+        i++
+    }
+    return waysForIdx[n]
+};
+
+/**
+ * note the method of 
+ * -- brute force recursion, calling function for smaller values
+ * -- optimise with memoisation
+ * -- use array to store previous (as above)
+ * -- actually only need two previous values, so can manage in O(1) space
+ */
+
+const tests = [1, 2, 8, 45, 46, 60]
+const answers = [1, 2, 34, 1836311903, 2971215073, 2504730781961];
+
+tests.forEach((t, i) => console.log(
+    climbStairs(t), 'should be', answers[i]
+));
