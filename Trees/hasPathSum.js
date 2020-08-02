f3a57586cd0aba6e0d2dfc2d5e02a9d813beb84a
Samir70 <sbetmouni@talktalk.net>
2020-08-02 06:44:38 +0100

    Tree.hasPathSum

---
+++ Trees/hasPathSum.js
@@ -0,0 +1,7 @@
+var hasPathSum = function(root, sum) {
+    if (root === null) {return false}
+    if (root.left === null && root.right === null) {
+        return root.val === sum
+    }
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+};
