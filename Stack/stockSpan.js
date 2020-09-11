4225690d879036dd44b59c6ad4db6cf566b9ea49
Samir70 <46189442+Samir70@users.noreply.github.com>
2020-09-11 16:06:23 +0100

    define stock span

--- Stack/stockSpan.js
+++ Stack/stockSpan.js
@@ -2,7 +2,9 @@
  * if the price of a stock over the next 7 days were
  *  [100, 80, 60, 70, 60, 75, 85], then the stock spans would be
  *  [1, 1, 1, 2, 1, 4, 6].
- 
+ The span of the stock's price today is defined as the maximum number of consecutive days 
+ (starting from today and going backwards) for which the price of the stock was 
+ less than or equal to today's price.
 */
 
 class StockSpanner {
