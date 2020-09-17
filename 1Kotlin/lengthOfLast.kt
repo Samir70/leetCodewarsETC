28cda95fb755a7ab1314c6fab2b0694fe5ec3e1a
Samir70 <sbetmouni@talktalk.net>
2020-09-17 21:18:01 +0100

    update kotlin length of last word

--- 1Kotlin/lengthOfLast.kt
+++ 1Kotlin/lengthOfLast.kt
@@ -1,8 +1,12 @@
 class Solution {
     fun lengthOfLastWord(s: String): Int {
-        val words = s.split(' ');
+        val words = s.trim(' ').split(' ');
         if (words.count() === 0) {return 0}
         print(words)
         return words[words.count() - 1].length
     }
 }
+
+/*
+tests: "hello world", "a ", "", "a"
+*/
