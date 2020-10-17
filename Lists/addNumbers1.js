27a2cf2150a3b5340833725172ca07e2f37098ba
Samir70 <sbetmouni@talktalk.net>
2020-10-17 15:12:16 +0100

    add numbers as linked lists

---
+++ Lists/addNumbers1.js
@@ -0,0 +1,24 @@
+// lists have numbers starting from least sig digit
+// to 764 is 4 -> 6 -> 7 -> null
+
+var addTwoNumbers = function (left, right) {
+    let a = left, b = right;
+    let sum = new ListNode();
+    let cur = sum;
+    let carry = 0;
+    while (a || b || carry === 1) {
+        let v = a === null ? 0 : a.val;
+        let w = b === null ? 0 : b.val;
+        let total = v + w + carry;
+        carry = total > 9 ? 1 : 0;
+        let s = new ListNode(total % 10, null)
+        cur.next = s;
+        cur = cur.next;
+        a = a === null ? a : a.next;
+        b = b === null ? b : b.next;
+    }
+    return sum.next
+};
+
+// includes lists of different lengths in the tests
+
