6ef0c8ddd3dcae4093920ff6a37d0dbd0d8854f4
Samir70 <sbetmouni@talktalk.net>
2021-02-10 21:40:02 +0000

    add deep copy of linked list with random link

---
+++ Lists/deepCopy.js
@@ -0,0 +1,38 @@
+const listVals = head => {
+    let cur = head;
+    while (cur) {
+        console.log(cur.val, cur.next && cur.next.val, cur.random && cur.random.val);
+        cur = cur.next;
+    }
+}
+var copyRandomList = function(head) {
+    let cur = head;
+    let newHead = cur ? new Node(cur.val, null, null) : null;
+    let newCur = newHead;
+    while (cur && cur.next) {
+        let newNode = new Node(cur.next.val);
+        newCur.next = cur.next;
+        cur.next = newCur;
+        cur = newCur.next;
+        newCur = newNode;
+    }
+    if (cur) {cur.next = newCur};
+    cur = head; newCur = newHead;
+    // listVals(head);
+    while (cur && cur.next) {
+        newCur.random = cur.random ? cur.random.next : null;
+        cur = cur.next.next;       
+        newCur = newCur.next ? newCur.next.next : null;
+    }
+    // listVals(head)
+    cur = head; newCur = newHead;
+    while (cur && cur.next) {
+        cur.next = cur.next.next;
+        newCur.next = newCur.next ? newCur.next.next : null;
+        cur = cur.next; newCur = newCur.next
+    }
+    // listVals(head)
+    return newHead
+};
+
+// https://leetcode.com/problems/copy-list-with-random-pointer/
