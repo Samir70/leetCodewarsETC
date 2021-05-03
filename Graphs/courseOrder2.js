767827c030b8cd9b5db7a221ad6defd248d443f9
Samir70 <sbetmouni@talktalk.net>
2021-05-03 18:29:59 +0100

    add course order 2

--- Graphs/courseOrder2.js
+++ Graphs/courseOrder2.js
@@ -32,6 +32,34 @@ var findOrder = function (n, deps) {
     return edgeCount > 0 ? [] : out
 };
 
+/**
+ * var findOrder = function(n, b2as) {
+    let noIns = new Set([...Array(n)].map((x, i) => i));
+    let ins = {}, outs = {}
+    for (let [a, b] of b2as) {
+        if (ins[a] === undefined) {ins[a] = 0} 
+        if (outs[b] === undefined) {outs[b] = new Set() }
+        ins[a]++;
+        outs[b].add(a)
+        noIns.delete(a)
+    }
+    let stack = Array.from(noIns);
+    let edgecount = b2as.length;
+    let out = [];
+    while (stack.length) {
+        let cur = stack.pop()
+        out.push(cur)
+        if (outs[cur] === undefined) {continue}
+        for (let dest of outs[cur]) {
+            ins[dest]--
+            if (ins[dest] === 0) {stack.push(dest)}
+            edgecount--
+        }
+    }
+    return edgecount ? [] : out
+};
+ */
+
 const tests = [
     { n: 2, prereq: [[1, 0]], out: [0, 1] },
     { n: 2, prereq: [[1, 0], [0, 1]], out: [] },
