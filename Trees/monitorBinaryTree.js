a11f68313ff5d62ec45b96333c35437eae26f31d
Samir70 <sbetmouni@talktalk.net>
2021-05-20 21:17:34 +0100

    add monitor binary tree

--- Trees/monitorBinaryTree.js
+++ Trees/monitorBinaryTree.js
@@ -12,3 +12,17 @@ var minCameraCover = function(root) {
     }
     return dfs(root) > 2 ? count + 1 : count
 };
+
+/**
+ * A val of 0 means that there's no pressing need to place a camera on this node 
+ * because nothing below needs one. On the other hand, 
+ * it also means that this node is unmonitored, 
+ * otherwise val would be at least a 1. 
+ * So rather than placing a camera here, we want to place one in the parent node. 
+ * That means that we send a 3 up to let the parent know that we need it to place a camera.
+
+A leaf node will have a val of 0, yes, because both of its children are null, 
+so it will send a 3 up to its parent. 
+But a node that is covered from below and does not need a camera will also send up a 0, 
+because its parent can basically treat it as if it's a null branch.
+ */
