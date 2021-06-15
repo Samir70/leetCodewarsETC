3995968353fd4c30f085b0999858fd7985217e20
Samir70 <sbetmouni@talktalk.net>
2021-06-15 17:47:54 +0100

    add random topic

---
+++ randomTopic.js
@@ -0,0 +1,11 @@
+const topics = [
+    "Array", "Dynamic Programming", "string", "Math", "Greedy", "Depth-tirst Search",
+    "Tree", "Hash Table", "Binary Search", "Breadth-frst Search", "Sort", "Two Pointers", "Backtracking",
+    "Design", "Stack", "Bit Manipulation", "Graph", "Heap", "Linked List", "Recursion", "Union Find", "Sliding Window",
+    "Trie", "Divide and Conquer", "Ordered Map", "Segment Tree", "Queue", "Geometry", "Line sweep", "Binary Indexed Tree",
+    "Minimax", "Brainteaser", "Topological Sort", "Deque", "Random", "Binary Search Tree", "Rolling Hash", "Suffix Array",
+    "Rejection Sampling", "Reservoir Sampling", "Meet in the Middle", "Memorization", "OOP"
+];
+
+let choice = Math.floor(Math.random() * topics.length);
+console.log(topics[choice])
