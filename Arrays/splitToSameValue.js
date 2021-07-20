bbea4247af62daa7d33f491e8a83d7a6e0219fa6
Samir70 <46189442+Samir70@users.noreply.github.com>
2021-07-20 14:31:29 +0100

    Update splitToSameValue.js

--- Arrays/splitToSameValue.js
+++ Arrays/splitToSameValue.js
@@ -1,4 +1,5 @@
-var threeEqualParts = function (arr) {
+// 136ms
+var threeEqualPartsSlow = function (arr) {
     // console.table(arr)
     let ones = arr.reduce((acc, val) => acc += val, 0)
     if (ones % 3) { return [-1, -1] }
@@ -37,6 +38,36 @@ var threeEqualParts = function (arr) {
     return [leftEnd, rightStart]
 };
 
+// 92ms, beats 71%
+var threeEqualParts = function(arr) {
+    let ones = 0;
+    for (let n of arr) {if (n === 1) {ones++}}
+    if (ones % 3 !== 0) {return [-1, -1]}
+    if (ones === 0) {return [0, arr.length - 1]}
+    let n = ones/3
+    
+    // figure out leftEnd and rightStart (i and j)
+    let starts = [], count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 1) {
+            count++;
+            if (count === 1) {starts.push(i)}
+        }
+        if (count === n) {count = 0}
+    } 
+    // console.log(arr)
+    // console.log(starts)
+    
+    let [a, b, c] = starts
+    while (c < arr.length) {
+        if (a === starts[1] || b === starts[2]) {return [-1, -1]}
+        if (arr[a] !== arr[b] || arr[b] !== arr[c]) {return [-1, -1]}
+        a++; b++; c++
+    }
+    // console.log([a, b, c])
+    return [a - 1, b]
+};
+
 
 const tests = [
     { arr: [1, 0, 1, 0, 1], out: [0, 3] },
@@ -46,7 +77,8 @@ const tests = [
     { arr: [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], out: [-1, -1] },
     { arr: [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], out: [-1, -1] },
     { arr: [1, 0, 1, 1, 0], out: [-1, -1] },
-    { arr: [0, 0, 0, 0, 0], out: [0, 4] }
+    { arr: [0, 0, 0, 0, 0], out: [0, 4] },
+    { arr: [0,1,0,1,1,0,1,1,0,1], out: [3, 7]}
 ];
 
 tests.forEach((t, i) => console.log(
