5ee863c9eba4c364b73a1848059c47cee7095d4a
Samir70 <46189442+Samir70@users.noreply.github.com>
2021-09-13 13:11:39 +0100

    Update q4-goodsubsets.js

--- LeetContests/BiWeekly60/q4-goodsubsets.js
+++ LeetContests/BiWeekly60/q4-goodsubsets.js
@@ -24,14 +24,11 @@ var numberOfGoodSubsets = function(nums) {
         if (count[c] === 0) {continue}
         if (c % 4 === 0 || c % 9 === 0 || c % 25 === 0) {continue}
         let mask = makeMask(c)
-        let i = -1
-        while (i < 10) {
-            bit = i === -1 ? 0 : 1 << i
-            i++
-            if ((i!== 0) && (bit & mask)) {continue}
-            dp[bit | mask] += count[c]*dp[bit]
-            dp[bit | mask] %= base
-            console.log([c, bit], dp[bit], mask, (bit | mask), dp[bit | mask])
+        for (let i = 0; i < 1024; i++) {
+            if (i & mask) {continue}
+            dp[i | mask] += count[c]*dp[i]
+            dp[i | mask] %= base
+            // console.log([c, i], dp[i], mask, (i | mask), dp[i | mask])
         }
     }
     
@@ -39,10 +36,13 @@ var numberOfGoodSubsets = function(nums) {
     for (let val of dp) {
         sum = (sum + val) % base
     }
-    console.log({count1:count[1], sum})
-    return count[1] > 0 ? (2**count[1])*sum : sum
+    // console.log({count1:count[1], sum})
+    let oneFactor = (2n ** BigInt(count[1]) * BigInt(sum)) % BigInt(base)
+    return count[1] > 0 ? oneFactor : sum
 };
 
 
 // [18,28,2,17,29,30,15,9,12] => 19
 // [4,2,3,15, 6, 6, 8, 13, 29, 24, 15, 1, 7] => 158
+// [10,11,5,1,10,1,3,1,26,11,6,1,1,15,1,7,22,1,1,1,1,1,23,1,29,5,6,1,1,29,1,1,21,19,1,1,1,2,1,11,1,15,1,22,14,1,1,1,1,6,7,1,14,3,5,1,22,1,1,1,17,1,29,2,1,15,10,1,5,7,1,1,1,30,1,30,1,21,10,1,1,1,1,1,2,6,5,7,3,1,1,19,29,1,7,13,14,1,5,26,19,11,1,1,1,1,1,1,1,1,22,15,1,1,13,1,17,1,1,1,13,6,1,10,1,1,17,1,1,3,14,7,17,1,13,1,1,1,1,1,11,1,1,6,1,1,1,1,1,2,1,30,2,26,1,1,14,1,26,29,30,1,13,21,1,1,14,21,1,23,1,15,23,21,1,30,19,19,1,10,23,3,3,17,22,2,26,1,11,1,23,1,1,1,15,1,1,13,1,1]
+// => 520317213
