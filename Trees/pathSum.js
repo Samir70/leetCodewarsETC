16e8e7541a79ddac52875f48bf3a241a9376678c
Samir70 <sbetmouni@talktalk.net>
2022-05-02 13:29:24 +0100

    update pathsum

--- Trees/pathSum.js
+++ Trees/pathSum.js
@@ -12,6 +12,23 @@ var pathSum3 = function(root, targetSum, canSkip = true) {
     // console.log({root, targetSum, canSkip, ways})
     return ways
 };
+// below also worked, without propogating canSkip
+var pathSum3b = function(root, sum, allowSkip = true) {
+    if (root === null) return 0
+    let out = 0
+    if (root.val === sum) {
+        out++ // short path!
+    }
+    // use the root
+    out += pathSum3b(root.left, sum - root.val, false)
+    out += pathSum3b(root.right, sum - root.val, false)
+    // don't use the root
+    if (allowSkip) {
+        out += pathSum3b(root.left, sum)
+        out += pathSum3b(root.right, sum)
+    }
+    return out
+};
 
 // 96 ms, faster than 37.19%
 // 49.6 MB, less than 25.93%
