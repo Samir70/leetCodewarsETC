df0d0e88c76ac5ee7fb21ca9bcc8ac67fa919df5
Samir70 <sbetmouni@talktalk.net>
2023-08-08 06:47:56 +0100

    searching a sorted, rotated array

---
+++ 2023-08/08findInSortedRotated.js
@@ -0,0 +1,73 @@
+var search1 = function (nums, target) {
+  let left = 0, right = nums.length - 1;
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+    // console.log(left, right, mid)
+    if (nums[mid] === target) { return mid }
+    let pivotToRight = nums[right] < nums[mid] ? true : false;
+    if (nums[mid] < target) {
+      if (pivotToRight) {
+        // every to the left of mid is sorted, so target cannot be there
+        left = mid + 1
+      } else {
+        // check if target is on the right, which is all sorted
+        if (target <= nums[right]) {
+          left = mid + 1
+        } else {
+          right = mid - 1
+        }
+      }
+    } else {
+      // now target <= nums[mid]
+      if (!pivotToRight) {
+        // everything to the right of mid is sorted, and bigger than mid value
+        right = mid - 1
+      } else {
+        // check if target is on left, which is all sorted
+        if (target >= nums[left]) {
+          right = mid - 1
+        } else {
+          left = mid + 1
+        }
+      }
+    }
+  }
+  return nums[left] === target ? left : -1
+};
+
+// Beats 42%
+const search = (nums, target) => {
+  let left = 0, right = nums.length - 1;
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) { return mid }
+    let tellTale = (nums[mid] < nums[0]) === (target < nums[0]) ? nums[mid] :
+      target < nums[0] ? -Infinity : Infinity;
+    if (tellTale < target) {
+      left = mid + 1
+    } else {
+      right = mid
+    }
+  }
+  return nums[left] === target ? left : -1
+}
+
+const tests = [
+  { args: [[4, 5, 6, 7, 0, 1, 2], 0], out: 4 },
+  { args: [[4, 5, 6, 7, 0, 1, 2], 3], out: -1 },
+  { args: [[1], 0], out: -1 },
+  { args: [[1], 1], out: 0 },
+  { args: [[1, 2], 2], out: 1 },
+  { args: [[1, 2, 3], 3], out: 2 }
+];
+
+tests.forEach((t, i) => {
+  let res = search(...t.args);
+  if (res !== t.out) {
+    console.log(
+      'test', i, 'should be', t.out, ' got ', res
+    )
+  } else {
+    console.log('test', i, 'was correct!')
+  }
+});
