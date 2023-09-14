6728249a8a7675f504e4a17e8b1c839b9da3b0da
Samir70 <sbetmouni@talktalk.net>
2023-09-14 06:51:44 +0100

    use faster algorithm for findItinerary

--- 2023-09/14findItinerary_OLD.js
+++ 2023-09/14findItinerary_OLD.js
@@ -1,33 +1,36 @@
-// This verseion beat 91%
+const wholepath = (path, g, n) => {
+  if (path.length === n) { return path }
+  var last = path[path.length - 1].split('-')[1];
+  if (g[last] === undefined) { return false }
+  var choices = g[last].filter(x => !path.includes(x));
+  for (var c of choices) {
+    var outcome = wholepath([...path, c], g, n);
+    // console.log('outcome for', [...path, c], 'is', outcome)
+    if (outcome !== false) { return outcome }
+  }
+  return false
+}
+
 var findItinerary = function (tickets) {
   var graph = {}
-  for (var [from, to] of tickets) {
+  for (var i in tickets) {
+    var [from, to] = tickets[i]
     if (graph[from] === undefined) {
       graph[from] = []
     }
-    if (graph[to] === undefined) {
-      graph[to] = []
-    }
-    graph[from].push(to)
+    graph[from].push(from + '-' + to + '-' + i)
   }
   for (from in graph) {
-    graph[from].sort().reverse();
+    graph[from].sort();
+    // console.log(graph[from])
   }
-  // console.log(graph)
-  let stack = ["JFK"]
-  let route = []
-  while (stack.length > 0) {
-    let cur = stack[stack.length - 1]
-    // console.log({ cur, stack, route })
-    if (graph[cur].length > 0) {
-      let dest = graph[cur].pop()
-      stack.push(dest)
-    } else {
-      stack.pop()
-      route.push(cur)
-    }
+  var journey = ['???-JFK'];
+  for (var ticket of graph["JFK"]) {
+    // console.log('starting search from', [...journey, ticket]);
+    var outcome = wholepath([...journey, ticket], graph, tickets.length + 1)
+    if (outcome !== false) return outcome.map(x => x.split('-')[1]).join(',').split(',')
   }
-  return route.reverse()
+  return "oops! didn't seem to find a journey".split(' ')
 };
 
 var tests = [
