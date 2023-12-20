bfc48e8f5e60cf46ec2748851e39b5f5c8b07626
Samir70 <sbetmouni@talktalk.net>
2023-12-20 07:45:30 +0000

    make buy chocs more efficient

--- 2023-12/20buyChocolate.js
+++ 2023-12/20buyChocolate.js
@@ -4,13 +4,26 @@
  * @return {number}
  */
 var buyChoco = function (prices, money) {
-  [cheap, cheapish] = prices.sort((a, b) => a - b)
-  return cheap + cheapish <= money ? money - cheap - cheapish : money
+  let a = Infinity, b = Infinity
+  for (let p of prices) {
+    if (p <= a) {
+      b = a;
+      a = p;
+    } else if (p <= b) {
+      b = p
+    }
+  }
+  return a + b <= money ? money - (a + b) : money
 };
+// var buyChoco = function (prices, money) {
+//   [cheap, cheapish] = prices.sort((a, b) => a - b)
+//   return cheap + cheapish <= money ? money - cheap - cheapish : money
+// };
 
 tests = [
   { args: [[1, 2, 2], 3], out: 0 },
   { args: [[3, 1, 3], 3], out: 3 },
+  { args: [[3, 1, 1], 3], out: 1 },
 ];
 
 tests.forEach((t, i) => {
