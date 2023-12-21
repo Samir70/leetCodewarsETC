5b17bb022576ea0665953cdaeab00fe7fccda18b
Samir70 <sbetmouni@talktalk.net>
2023-12-21 09:24:50 +0000

    optimise max width

--- 2023-12/21maxWidth.js
+++ 2023-12/21maxWidth.js
@@ -2,18 +2,18 @@
  * @param {number[][]} points
  * @return {number}
  */
-var maxWidthOfVerticalArea = function(points) {
-  let ps = points.map(p => p[0]).sort((a, b) => a - b)
+var maxWidthOfVerticalArea = function (points) {
+  points.sort((a, b) => a[0] - b[0])
   let widest = 0
-  for (let i = 1; i < ps.length; i++) {
-      widest = Math.max(widest, ps[i] - ps[i - 1])
-  }  
+  for (let i = 1; i < points.length; i++) {
+    widest = Math.max(widest, points[i][0] - points[i - 1][0])
+  }
   return widest
 };
 
 const tests = [
-  {args: [[[8,7],[9,9],[7,4],[9,7]]], out: 1}, 
-  {args: [[[3,100],[9,330],[1,330],[1,334],[5,3333],[8,338]]], out: 3}, 
+  { args: [[[8, 7], [9, 9], [7, 4], [9, 7]]], out: 1 },
+  { args: [[[3, 100], [9, 330], [1, 330], [1, 334], [5, 3333], [8, 338]]], out: 3 },
 ];
 
 tests.forEach((t, i) => {
