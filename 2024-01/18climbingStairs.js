7d9c99c9de4cf37acd206138afdc2fd9e268574a
Samir70 <sbetmouni@talktalk.net>
2024-01-18 06:19:20 +0000

    climbing stairs

--- 2024-01/18climbingStairs.js
+++ 2024-01/18climbingStairs.js
@@ -1,28 +1,28 @@
 // How many ways to climb n steps 
 // if you can take either one or two steps each turn
 var climbStairs = function (n) {
-    var waysForIdx = [0, 1, 2];
-    var i = 3;
-    while (i <= n) {
-        var afterOneStep = waysForIdx[i - 1];
-        var afterTwoSteps = waysForIdx[i - 2];
-        waysForIdx.push(afterOneStep + afterTwoSteps);
-        i++
-    }
-    return waysForIdx[n]
+  var waysForIdx = [0, 1, 2];
+  var i = 3;
+  while (i <= n) {
+    var afterOneStep = waysForIdx[i - 1];
+    var afterTwoSteps = waysForIdx[i - 2];
+    waysForIdx.push(afterOneStep + afterTwoSteps);
+    i++
+  }
+  return waysForIdx[n]
 };
 
 // use less memory, since you only need the last two elements
 // beats 96% on speed, but only 13% on mem
 var climbStairs = function (n) {
-    if (n <= 2) { return n }
-    let [a, b] = [1, 2];
-    let i = 2;
-    while (i < n) {
-        [a, b] = [b, a + b];
-        i++
-    }
-    return b
+  if (n <= 2) { return n }
+  let [a, b] = [1, 2];
+  let i = 2;
+  while (i < n) {
+    [a, b] = [b, a + b];
+    i++
+  }
+  return b
 };
 
 /**
@@ -37,5 +37,5 @@ const tests = [1, 2, 8, 45, 46, 60]
 const answers = [1, 2, 34, 1836311903, 2971215073, 2504730781961];
 
 tests.forEach((t, i) => console.log(
-    climbStairs(t), 'should be', answers[i]
+  climbStairs(t), 'should be', answers[i]
 ));
