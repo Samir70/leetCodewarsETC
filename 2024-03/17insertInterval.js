cd15a59919b56898b945867c68a233efb64f14e2
Samir70 <sbetmouni@talktalk.net>
2024-03-17 06:02:46 +0000

    one pass insert interval, no sorting

--- 2024-03/17insertInterval.js
+++ 2024-03/17insertInterval.js
@@ -3,24 +3,43 @@
  * @param {number[]} newInterval
  * @return {number[][]}
  */
+// 73 ms
 var insert = function (intervals, newInterval) {
-  intervals.push(newInterval)
-  intervals.sort((a, b) => a[0] - b[0])
-  let cur = intervals[0]
+  let n = intervals.length, i = 0;
   let out = []
-  for (let i = 1; i < intervals.length; i++) {
-    let [start, end] = intervals[i]
-    if (start > cur[1]) {
-      out.push(cur)
-      cur = [start, end]
-    } else {
-      cur[1] = Math.max(cur[1], end)
-    }
-    // console.log({start, end, cur, out})
+  while (i < n && intervals[i][1] < newInterval[0]) {
+    out.push(intervals[i++])
   }
-  out.push(cur)
+  while (i < n && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0])
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1])
+    i++
+  }
+  out.push(newInterval)
+  while (i < n) {
+    out.push(intervals[i++])
+  }
   return out
 };
+// 72 and 82ms
+// var insert = function (intervals, newInterval) {
+//   intervals.push(newInterval)
+//   intervals.sort((a, b) => a[0] - b[0])
+//   let cur = intervals[0]
+//   let out = []
+//   for (let i = 1; i < intervals.length; i++) {
+//     let [start, end] = intervals[i]
+//     if (start > cur[1]) {
+//       out.push(cur)
+//       cur = [start, end]
+//     } else {
+//       cur[1] = Math.max(cur[1], end)
+//     }
+//     // console.log({start, end, cur, out})
+//   }
+//   out.push(cur)
+//   return out
+// };
 
 const tests = [
   { args: [[[1, 3], [6, 9]], [2, 5]], out: [[1, 5], [6, 9]] },
@@ -29,7 +48,7 @@ const tests = [
 
 tests.forEach((t, i) => {
   let res = insert(...t.args);
-  if (res !== t.out) {
+  if (res.join(",") !== t.out.join(",")) {
     console.log(
       'test', i, 'should be', t.out, ' got ', res
     )
