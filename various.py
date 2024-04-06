e480e22afc999371b4f0e6839fdb31333d703162
Samir70 <sbetmouni@talktalk.net>
2024-04-06 10:09:12 +0100

    concatinate tables

--- various.py
+++ various.py
@@ -4,3 +4,7 @@ import pandas as pd
 def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
     employees['bonus'] = employees['salary'] * 2
     return employees
+
+# https://leetcode.com/problems/reshape-data-concatenate/
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2])
