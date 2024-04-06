be3a9b9f853c637210374e363867cae9f6a0adfc
Samir70 <sbetmouni@talktalk.net>
2024-04-06 10:09:38 +0100

    clarify purpose

--- various.py
+++ various.py
@@ -6,5 +6,6 @@ def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
     return employees
 
 # https://leetcode.com/problems/reshape-data-concatenate/
+# rows of df2 added to df1
 def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return pd.concat([df1, df2])
