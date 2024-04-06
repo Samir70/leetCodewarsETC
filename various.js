31f859ac7d85865b5c32a48cf437287b3806e18c
Samir70 <sbetmouni@talktalk.net>
2024-04-06 10:17:35 +0100

    find value after ops

--- various.js
+++ various.js
@@ -6,13 +6,13 @@
 var mySqrt = function (x) {
   let left = 0, right = x;
   while (left < right) {
-      let mid = left + Math.ceil((right - left) / 2)
-      if (mid * mid > x) {
-          // ans is in [left, mid)
-          right = mid - 1
-      } else {
-          left = mid
-      }
+    let mid = left + Math.ceil((right - left) / 2)
+    if (mid * mid > x) {
+      // ans is in [left, mid)
+      right = mid - 1
+    } else {
+      left = mid
+    }
   }
   return left
 };
@@ -24,9 +24,26 @@ var mySqrt = function (x) {
  * @return {Promise}
  * https://leetcode.com/problems/add-two-promises/
  */
-var addTwoPromises = async function(promise1, promise2) {
+var addTwoPromises = async function (promise1, promise2) {
   out = await Promise.all([promise1, promise2]).then(vals => {
-      return vals[0] + vals[1]
+    return vals[0] + vals[1]
   })
   return out
+};
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ * https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+ */
+var finalValueAfterOperations = function (operations) {
+  out = 0
+  for (let op of operations) {
+    out += op.indexOf("+") > -1 ? 1 : -1
+  }
+  return out
+};
+var finalValueAfterOperations = function (operations) {
+  return operations.map(op => op.indexOf("+") === -1 ? -1 : 1)
+    .reduce((a, c) => a + c, 0)
 };
