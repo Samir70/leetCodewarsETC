39232455e74a5fa9b4aa719a8114a176d19d4992
Samir70 <sbetmouni@talktalk.net>
2024-05-31 06:03:11 +0100

    two single numbers

--- 2024-05/31singleNumber3.js
+++ 2024-05/31singleNumber3.js
@@ -3,19 +3,19 @@
  * The rest appear twice
  */
 
-var singleNumber3 = function(nums) {
-    var tally = {}
-    for (var n of nums) {
-        if (tally[n] === undefined) {
-            tally[n] = 0
-        }
-        tally[n]++
+var singleNumber3 = function (nums) {
+  var tally = {}
+  for (var n of nums) {
+    if (tally[n] === undefined) {
+      tally[n] = 0
     }
-    var out = []
-    for (var t in tally) {
-        if (tally[t] === 1) {out.push(t)}
-    }
-    return out
+    tally[n]++
+  }
+  var out = []
+  for (var t in tally) {
+    if (tally[t] === 1) { out.push(t) }
+  }
+  return out
 };
 
 /**
@@ -42,22 +42,39 @@ by XoRing all numbers in the second group, we can get the second number.
 nb: x & (~x + 1) isolates rightmost bit
  */
 
-const singleNumber = nums => {
-    var xor = nums.reduce((a, v) => a^v, 0);
-    var rightmost = xor & (~xor + 1);
-    var a = nums.filter(x => x&rightmost).reduce((a, v) => a^v, 0) 
-    var b = nums.filter(x => !x&rightmost).reduce((a, v) => a^v, 0) 
-    return [a, b]
+var singleNumber = nums => {
+  var xor = nums.reduce((a, v) => a ^ v, 0);
+  var rightmost = xor & (~xor + 1);
+  var a = nums.filter(x => x & rightmost).reduce((a, v) => a ^ v, 0)
+  var b = nums.filter(x => !x & rightmost).reduce((a, v) => a ^ v, 0)
+  return [a, b]
 }
 // NB only need the first set since b = xor^a 
 
 // I thought this would be faster but it was slower:
-var singleNumber = function(nums) {
-    let xorOfAll = nums.reduce((a, c) => a^c, 0);
-    let rightmost = xorOfAll & (~xorOfAll + 1);
-    let firstNum = 0;
-    for (let n of nums) {
-        if (n & rightmost) { firstNum = firstNum ^ n}
-    }
-    return [firstNum, xorOfAll^firstNum]
-};
+// var singleNumber = function(nums) {
+//     let xorOfAll = nums.reduce((a, c) => a^c, 0);
+//     let rightmost = xorOfAll & (~xorOfAll + 1);
+//     let firstNum = 0;
+//     for (let n of nums) {
+//         if (n & rightmost) { firstNum = firstNum ^ n}
+//     }
+//     return [firstNum, xorOfAll^firstNum]
+// };
+
+const tests = [
+  { args: [[1, 2, 1, 3, 2, 5]], out: [3, 5] },
+  { args: [[-1, 0]], out: [-1, 0] },
+  { args: [[0, 1]], out: [0, 1] },
+]
+
+tests.forEach((t, i) => {
+  let res = singleNumber(...t.args);
+  if (res !== t.out) {
+    console.log(
+      'test', i, 'should be', t.out, ' got ', res
+    )
+  } else {
+    console.log('test', i, 'was correct!')
+  }
+});
