cf78ca0ea774af0303c3502b142dc5b6f801fd03
Samir70 <sbetmouni@talktalk.net>
2024-06-18 07:01:15 +0100

    max profit, linear time

--- 2024-06/18maxProfit_sorting.js
+++ 2024-06/18maxProfit_sorting.js
@@ -5,22 +5,21 @@
  * @return {number}
  */
 var maxProfitAssignment = function (difficulty, profit, worker) {
-  let maxAbility = Math.max(...worker)
-  let jobs = Array(maxAbility + 1).fill(0)
-  for (let i = 0; i < difficulty.length; i++) {
-    if (difficulty[i] > maxAbility) { continue }
-    jobs[difficulty[i]] = Math.max(jobs[difficulty[i]], profit[i])
+  let diffPro = []
+  for (let i = 0; i < profit.length; i++) {
+    diffPro.push([difficulty[i], profit[i]])
   }
-  let maxProfit = 0
-  for (let j = 0; j < jobs.length; j++) {
-    maxProfit = Math.max(maxProfit, jobs[j])
-    jobs[j] = maxProfit
-  }
-  // console.log(maxAbility, jobs)
-  let moneyMade = 0
+  diffPro.sort((a, b) => a[0] - b[0])
+  worker.sort((a, b) => a - b)
+  // console.log(diffPro)
+  let moneyMade = 0, job = -1, bestProfit = 0
   for (let w of worker) {
-    moneyMade += jobs[w]
-    // console.log({ w, p: jobs[w], moneyMade })
+    while (job + 1 < diffPro.length && diffPro[job + 1][0] <= w) {
+      job++
+      bestProfit = Math.max(bestProfit, diffPro[job][1])
+    }
+    // console.log({ w, job, bestProfit })
+    moneyMade += bestProfit
   }
   return moneyMade
 };
