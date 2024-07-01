b4201fcb79963654406273e0030c3ee57dd38c2a
Samir70 <sbetmouni@talktalk.net>
2024-07-01 06:44:35 +0100

    three consecutive odds

--- various.js
+++ various.js
@@ -216,3 +216,21 @@ var heightChecker = function (heights) {
   }
   return count
 };
+
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function (arr) {
+  let count = 0
+  for (let n of arr) {
+    if (n % 2) {
+      count++
+      if (count === 3) { return true }
+    } else {
+      count = 0
+    }
+  }
+  return false
+};
