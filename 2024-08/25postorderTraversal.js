5f1d1fbe714bdbe92deac66e636b1bf833e9c676
Samir70 <sbetmouni@talktalk.net>
2024-08-25 04:56:48 +0100

    postorder traversal

--- 2024-08/25postorderTraversal.js
+++ 2024-08/25postorderTraversal.js
@@ -1,5 +1,3 @@
-const trees = require('./trees');
-
 // easier backwards
 const postOrder = root => {
     var out = [];
@@ -19,6 +17,23 @@ const postOrder = root => {
     return out
 }
 
-for (var t in trees) {
-    console.log(postOrder(trees[t]))
-}
+// for (var t in trees) {
+//     console.log(postOrder(trees[t]))
+// }
+
+var postorderTraversal = function(root) {
+  let result = [];
+  
+  function traverse(node)
+  {
+      if(node === null)
+          return 0;
+      
+      traverse(node.left);
+      traverse(node.right);
+      result.push(node.val);
+  }
+  
+  traverse(root);
+  return result;
+};
