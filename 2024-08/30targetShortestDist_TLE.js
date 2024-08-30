40e82a9b84aa4ae80116702e834d6855cd3e9913
Samir70 <sbetmouni@talktalk.net>
2024-08-30 12:13:31 +0100

    target shortest dist, slow but passes

--- 2024-08/30targetShortestDist_TLE.js
+++ 2024-08/30targetShortestDist_TLE.js
@@ -1,54 +1,3 @@
-class Heap {
-  // minHeap by default, use -ve for maxHeap
-  constructor() {
-    this.size = 0;
-    this.arr = [null];
-  }
-
-  peek() { return this.size > 0 ? this.arr[1] : null }
-
-  bubbleUp(child) {
-    if (child > 1) {
-      var parent = Math.floor(child / 2);
-      if (this.arr[child] < this.arr[parent]) {
-        var temp = this.arr[child];
-        this.arr[child] = this.arr[parent];
-        this.arr[parent] = temp
-        this.bubbleUp(parent)
-      }
-    }
-  }
-
-  add(n) {
-    this.size++
-    this.arr[this.size] = n
-    this.bubbleUp(this.size)
-  }
-
-  bubbleDown(parent) {
-    var child = [this.arr[parent], parent];
-    var le = 2 * parent, r = 2 * parent + 1;
-    var left = le > this.size ? Infinity : this.arr[le]
-    if (left < child[0]) { child = [left, le] }
-    var right = r > this.size ? Infinity : this.arr[r];
-    if (right < child[0]) { child = [right, r] }
-    if (child[1] > parent) {
-      var temp = this.arr[parent];
-      this.arr[parent] = child[0];
-      this.arr[child[1]] = temp;
-      this.bubbleDown(child[1])
-    }
-  }
-
-  poll() {
-    var out = this.arr[1];
-    this.arr[1] = this.arr[this.size];
-    this.arr.pop()
-    this.size--
-    this.bubbleDown(1);
-    return out;
-  }
-}
 /**
  * @param {number} n
  * @param {number[][]} edges
@@ -76,30 +25,21 @@ var modifiedGraphEdges = function (n, edges, source, destination, target) {
       return minCost[a][b] === undefined ? Infinity : minCost[a][b]
     }
     if (getMinCost(a, b) !== Infinity) { return getMinCost(a, b) }
-    let nodesWithGivenCost = { 0: [a] }
-    let pq = new Heap()
-    pq.add(0)
+    let stack = [[0, a]]
     if (minCost[a] === undefined) { minCost[a] = {} }
     let visited = new Set()
-    while (pq.size) {
-      let dist = pq.poll()
-      if (dist > target) { return dist }
-      if (nodesWithGivenCost[dist] === undefined) { continue }
-      if (nodesWithGivenCost[dist].length === 0) { continue }
-      let cur = nodesWithGivenCost[dist].pop()
+    while (stack.length) {
+      let [dist, cur] = stack.pop()
       if (visited.has(cur)) { continue }
       minCost[a][cur] = Math.min(dist, getMinCost(a, cur))
+      // console.log({ a, cur, dist: getMinCost(a, cur) })
+      if (cur === b) { return dist }
       visited.add(cur)
-      if (Number(cur) === b) { return dist }
-      // console.log({a, dist, cur, minDis:minCost[a][cur]})
       for (let neighbour in graph[cur]) {
         if (visited.has(neighbour)) { continue }
-        // console.log({a, dist, cur, neighbour})
-        let newDist = dist + graph[cur][neighbour]
-        pq.add(newDist)
-        if (nodesWithGivenCost[newDist] === undefined) { nodesWithGivenCost[newDist] = [] }
-        nodesWithGivenCost[newDist].push(neighbour)
+        stack.push([dist + graph[cur][neighbour], neighbour])
       }
+      stack.sort((a, b) => b[0] - a[0])
     }
     return getMinCost(a, b)
   }
@@ -118,7 +58,7 @@ var modifiedGraphEdges = function (n, edges, source, destination, target) {
     if (c > 0) { continue }
     edges[i][2] = 1
     dist = dijkstra(source, destination)
-    // console.log({ i, dist, target })
+    // console.log({ i, dist })
     if (dist <= target) {
       edges[i][2] += target - dist
       for (let j = i + 1; j < edges.length; j++) {
@@ -149,7 +89,6 @@ const tests = [
 ];
 
 tests.forEach((t, i) => {
-  // if (i !== 2) { return }
   let res = modifiedGraphEdges(...t.args);
   if (res.join(",") !== t.out.join(",")) {
     console.log(
